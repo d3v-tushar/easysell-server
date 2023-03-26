bcc500075de8bea631ef56cc43d5e10133fec5d5
d3v-tushar <d3v.tushar@gmail.com>
2023-03-26 06:20:12 +0600

    Server Initilized & DB Intrigated

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+//const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 4000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@learnph.159fxoq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+const run = async () => {
+  try {
+    //Database
+    const productsCollection = client.db("easysell-pos").collection("products");
+
+    //APIs
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const products = await productsCollection.find(query).toArray();
+      res.send(products);
+    });
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+};
+run().catch((error) => console.error(error));
+
+app.get("/", (req, res) => {
+  res.send("EasySell Server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`EasySell Server is Running On Port ${port}`);
+});
